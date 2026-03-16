e20f94e027cff14593759358603c9d5ccd6d0d86
KShrus17 <shrushtikharva@gmail.com>
2026-03-15 20:15:33 -0700

    Shrushti - Function_4.cpp

--- Function_4.cpp
+++ Function_4.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
 using namespace std;
 
-//define necessary functions
-int main()
-{
-
-  //your code goes here
-
-return 0;
+// Function to compute the number of coins of a given denomination
+void computeCoins(int coinValue, int& num, int& amountLeft) {
+    num = amountLeft / coinValue;
+    amountLeft -= num * coinValue;
 }
 
-//implement functions
+int main() {
+    char ans;
+    do {
+        int amount;
+        cout << "Enter amount of cents (1-99): ";
+        cin >> amount;
+
+        int quarters, dimes, pennies;
+        int amountLeft = amount;
+
+        // Compute coins
+        computeCoins(25, quarters, amountLeft);
+        computeCoins(10, dimes, amountLeft);
+        computeCoins(1, pennies, amountLeft);
+
+        // Output result
+        cout << amount << " cents can be given as ";
+        cout << quarters << " quarter(s), ";
+        cout << dimes << " dime(s), and ";
+        cout << pennies << " penny(pennies)." << endl;
+
+        cout << "Enter Y or y to continue, any other to halt: ";
+        cin >> ans;
+
+    } while (ans == 'Y' || ans == 'y');
+
+    return 0;
+}
