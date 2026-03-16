67eef385a79494430cf9b3a856963325c9e2cc33
KShrus17 <shrushtikharva@gmail.com>
2026-03-15 19:39:53 -0700

    Shrushti - Function_1.cpp

--- Function_1.cpp
+++ Function_1.cpp
@@ -2,14 +2,34 @@
 #include <cstdlib>
 using namespace std;
 
-/* Define your function here */ 
+/* Define your function here */
+string CoinFlip() {
+   int flip = rand() % 2;   // generates 0 or 1
+
+   if (flip == 1) {
+      return "Heads";
+   }
+   else {
+      return "Tails";
+   }
+}
 
 int main() {
-   // Add more variables as needed
+   int numFlips;
    
    srand(2);  // Unique seed
 
-   /* Type your code here */
+   cin >> numFlips;
+
+   for (int i = 0; i < numFlips; i++) {
+      cout << CoinFlip();
+
+      if (i < numFlips - 1) {
+         cout << " ";
+      }
+   }
+
+   cout << endl;
 
    return 0;
 }
